5e73ad95a7b5d64bc792533b829a9f9468cb8625
Ali Vidler <alisonvidler@gmail.com>
2025-06-20 00:23:06 -0400

    Made the accordion work

--- src/Accordion.jsx
+++ src/Accordion.jsx
@@ -1,19 +1,28 @@
 import { React, useState } from 'react';
 
 const Accordion = () => {
+    const [isOpen1, setIsOpen1] = useState(false);
+    const [isOpen2, setIsOpen2] = useState(false);
 
     return (
         <div>
             <div className='accordion'>
-                <button>+</button>
-                Let's suppose we're working on our FriendlyGreeting component. We want to greet the user, but there's a problem: We don't know everyone's name.
+                <button onClick={() => setIsOpen1(!isOpen1)}>+</button>
+                 
+                {isOpen1 ? (
+                    <div>Let's suppose we're working on our FriendlyGreeting component.</div>
+    
+                ) : <div>We want to greet the user, but there's a problem: We don't know everyone's name.</div>}
             </div>
             <div className='accordion'>
-                <button>+</button>
-                I ran into this exact problem when I was building a tool to generate newsletter issues. I didn't know the name of every subscriber. If I didn't know their name, I wanted to render a “fallback” value:
+                <button onClick={() => setIsOpen2(!isOpen2)}>+</button>
+                {isOpen2 ? (
+                    <div>I ran into this exact problem when I was building a tool to generate newsletter issues.</div>
+                ) : <div>I didn't know the name of every subscriber. If I didn't know their name, I wanted to render a “fallback” value:</div> }
             </div>
-        </div>
+        </div>    
     )
+    
 }
 
 export default Accordion;
